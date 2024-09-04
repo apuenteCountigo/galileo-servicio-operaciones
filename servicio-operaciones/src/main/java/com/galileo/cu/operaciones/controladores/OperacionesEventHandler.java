173a34c1641a6baef94987a1eae675bd37071efc
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-09-04 11:08:56 -0400

    comentando orquestador

--- servicio-operaciones/src/main/java/com/galileo/cu/operaciones/controladores/OperacionesEventHandler.java
+++ servicio-operaciones/src/main/java/com/galileo/cu/operaciones/controladores/OperacionesEventHandler.java
@@ -87,14 +87,16 @@ public class OperacionesEventHandler {
 			throw new RuntimeException("Fallo Antes de Crear la Operación Validando Autorización: ");
 		}
 
-		try {
-			this.orquestador.iniciarCreacionOperacion(operaciones);
-			if (operaciones != null) {
-				throw new RuntimeException("Fallo Antes de Crear la Operación iniciando orquestador ");
-			}
-		} catch (Exception e) {
-			throw new RuntimeException("Fallo al iniciarCreacionOperacion: " + e.getMessage());
-		}
+		// try {
+		// this.orquestador.iniciarCreacionOperacion(operaciones);
+		// if (operaciones != null) {
+		// throw new RuntimeException("Fallo Antes de Crear la Operación iniciando
+		// orquestador ");
+		// }
+		// } catch (Exception e) {
+		// throw new RuntimeException("Fallo al iniciarCreacionOperacion: " +
+		// e.getMessage());
+		// }
 
 		try {
 			CrearDirectorios(operaciones);
