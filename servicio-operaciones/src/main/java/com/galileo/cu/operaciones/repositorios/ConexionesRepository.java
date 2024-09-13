bf3f1fb2d48eeb77168ec4ce013c02acc9efa3f5
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-09-13 07:28:10 -0400

    RECOMPILANDO-02

--- servicio-operaciones/src/main/java/com/galileo/cu/operaciones/repositorios/ConexionesRepository.java
+++ servicio-operaciones/src/main/java/com/galileo/cu/operaciones/repositorios/ConexionesRepository.java
@@ -10,6 +10,6 @@ import com.galileo.cu.commons.models.Conexiones;
 
 @RestResource(exported = false)
 public interface ConexionesRepository extends CrudRepository<Conexiones, Long> {
-    @Query("SELECT c FROM ConexionesFix c WHERE c.servicio=:servicio")
+    @Query("SELECT c FROM Conexiones c WHERE c.servicio=:servicio")
     public List<Conexiones> buscarFtp(String servicio);
 }
