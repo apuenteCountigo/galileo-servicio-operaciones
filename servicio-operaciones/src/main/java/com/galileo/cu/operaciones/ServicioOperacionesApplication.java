aaaeb7ed782547953a39a0cd6dfff9c1741fe384
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-09-16 13:23:34 -0400

    1-test ftp

--- servicio-operaciones/src/main/java/com/galileo/cu/operaciones/ServicioOperacionesApplication.java
+++ servicio-operaciones/src/main/java/com/galileo/cu/operaciones/ServicioOperacionesApplication.java
@@ -20,7 +20,7 @@ public class ServicioOperacionesApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("Operaciones V-2408280620 RECOMPILADA-2409140917");
+		System.out.println("Operaciones V-2408280620 RECOMPILADA-24091613:20");
 	}
 
 }
