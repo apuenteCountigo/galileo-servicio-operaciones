f17fd3a0e46fb2f566261816062f77ad2d8a2185
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-10-02 14:49:52 -0400

    6- Finding the error ftp

--- servicio-operaciones/src/main/java/com/galileo/cu/operaciones/repositorios/ConexionesRepository.java
+++ servicio-operaciones/src/main/java/com/galileo/cu/operaciones/repositorios/ConexionesRepository.java
@@ -1,8 +1,5 @@
 package com.galileo.cu.operaciones.repositorios;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
@@ -10,8 +7,5 @@ import com.galileo.cu.commons.models.Conexiones;
 
 @RestResource(exported = false)
 public interface ConexionesRepository extends CrudRepository<Conexiones, Long> {
-    @Query("SELECT c FROM Conexiones c WHERE c.servicio=:servicio")
-    public List<Conexiones> buscarFtp(String servicio);
-
     Conexiones findFirstByServicioContaining(String servicio);
 }
