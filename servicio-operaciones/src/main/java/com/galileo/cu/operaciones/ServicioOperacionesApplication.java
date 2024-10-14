043d976b940435b56a3c32536a1a5c2ec1f14039
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-10-14 05:51:11 -0400

    FIX EDIT AND DELETE WHIT IDS APIS NULL

--- servicio-operaciones/src/main/java/com/galileo/cu/operaciones/ServicioOperacionesApplication.java
+++ servicio-operaciones/src/main/java/com/galileo/cu/operaciones/ServicioOperacionesApplication.java
@@ -20,7 +20,7 @@ public class ServicioOperacionesApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("Operaciones V1.1-24-10-14 05:38");
+		System.out.println("Operaciones V1.1-24-10-14 05:50");
 	}
 
 }
