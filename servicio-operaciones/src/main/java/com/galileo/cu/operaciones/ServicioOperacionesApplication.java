b529bdfc778badac98ed1ddfaebdc0dcc439482c
apuentes+1-at-178934116267 <apuente@soaint.com>
2025-02-13 08:17:33 -0500

    VERSION 1.1.6

--- servicio-operaciones/src/main/java/com/galileo/cu/operaciones/ServicioOperacionesApplication.java
+++ servicio-operaciones/src/main/java/com/galileo/cu/operaciones/ServicioOperacionesApplication.java
@@ -20,7 +20,7 @@ public class ServicioOperacionesApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("Operaciones V1.1.5 25-01-17 11:00");
+		System.out.println("Operaciones V1.1.6 25-02-13 08:17");
 	}
 
 }
